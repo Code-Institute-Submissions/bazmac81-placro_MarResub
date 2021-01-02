8a541032f1bdcfd1aabf7a40488fb90aa6beaeac
bazmac81 <bazmac81@gmail.com>
2021-01-02 17:49:26 +0000

    Add: inlcude function to save stats to memory and load next page

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,7 +1,6 @@
 // Calculate Calories and macros using user stats and goal choice
 function macrosCalc(cals, g) {
     var protMulti, carbMulti, fatMulti, protein, carbs, fat = 0;
-
     if (g === "build"){
         protMulti = 0.25;
         carbMulti = 0.55;
@@ -18,14 +17,12 @@ function macrosCalc(cals, g) {
         protMulti = 0.3;
         carbMulti = 0.45;
         fatMulti = 0.25;
-    }
-
+    };
     protein = Math.round((cals * protMulti) / 4);
     carbs = Math.round((cals * carbMulti) / 4);
     fat = Math.round((cals * fatMulti) / 9);
 
-    var stats = [cals, protein, carbs, fat];
-    return stats;
+    return [cals, protein, carbs, fat];
 };
 
 // Using user stats calculate Base Metabolic Rate
@@ -41,36 +38,42 @@ function bmrCalc(a, w, h) {
     }
     else {
         return "Please provide all of your stats as numbers.";
-    }
+    };
 };
 
 // Take BMR and return with daily calorie amount
 function tdeeCalc(bmr){
     tdee = Math.ceil(bmr * 1.5);
-
     return tdee;
 };
 
 // Submit initial form. Hide seciton and reveal goals section.
 function statsCapture(e){
     e.preventDefault();
-    name = document.getElementById('name').value;
-    age = document.getElementById('age').value;
-};
+    n = document.getElementById('name');
+    a = document.getElementById('age'); 
+    h = document.getElementById('height');
+    w = document.getElementById('weight');
 
-// Capture goal selection. Store user data locally and proceed to next page
-function getGoal(e){
-    e.preventDefault();
-    
-    var goalOptions = document.getElementsByName('goals');
-    var goalChoice = "maintain";
-
-    for (i = 0; i < goalOptions.length; i++) {
-        if (goalOptions[i].checked == true){
-            goalChoice = goalOptions[i].value;
-        }
+    if (n.value != ""){
+        name = n.value;
     };
-    return goalChoice;
+    if (a.value != "") {
+        age = parseInt(a.value);
+    };
+    if (h.value != "") {
+        height = parseInt(h.value);
+    };
+    if (w.value != "") {
+        weight = parseInt(w.value);
+    };
+
+    if(measure ==="imperial"){
+        height = Math.round(height * inchMulti);
+        weight = Math.round(weight / lbsMulti);
+    }
+    
+    calories = tdeeCalc(bmrCalc(age, weight, height));
 };
 
 // Manage user choice for measurement and update any inputs to reflect.
@@ -78,9 +81,10 @@ function getMeasure(e){
     var m = e.target;
     var h = document.getElementById('height');
     var w = document.getElementById('weight');
-    var measure = m.getAttribute('id');
 
-    if(measure == "measure-met"){
+    measure = m.getAttribute('value');
+
+    if(measure === "metric"){
         if(w.value != "") {
             w.value = Math.round(w.value / lbsmulti);
         };
@@ -91,23 +95,50 @@ function getMeasure(e){
         document.getElementById('height').setAttribute('placeholder', 'Height cm');
     }
     else {
-        // Keep metric versions of weight and height for calculation but display imperial values in form
+        // Display imperial values in form
         if(w.value != "") {
-            weight = w.value;
             w.value = Math.round(w.value * lbsmulti);
         };
         if (h.value != "") {
-            height = h.value;
             h.value = Math.round(h.value / inchmulti);
         };
         document.getElementById('weight').setAttribute('placeholder', 'Weight lbs');
         document.getElementById('height').setAttribute('placeholder', 'Height ins');
     };
-}
+};
 
-const lbsmulti = 2.20462;
-const inchmulti = 2.54;
-var height, weight, name, age;
+function submitStats(e){
+    e.preventDefault();
+
+    var goalOptions = document.getElementsByName('goals');
+    for (i = 0; i < goalOptions.length; i++) {
+        if (goalOptions[i].checked === true) {
+            goal = goalOptions[i].value;
+        }
+    };
+    
+    stats = macrosCalc(calories, goal);
+
+    storeStats();
+
+    window.location.assign("../stats.html");
+};
+
+function storeStats(){
+    sessionStorage.setItem('name', name);
+    sessionStorage.setItem('age', age);
+    sessionStorage.setItem('measure', measure);
+    sessionStorage.setItem('goal', goal);
+    sessionStorage.setItem('calories', stats[0]);
+    sessionStorage.setItem('protien', stats[1]);
+    sessionStorage.setItem('carbs', stats[2]);
+    sessionStorage.setItem('fat', stats[3]);
+};
+
+const lbsMulti = 2.20462;
+const inchMulti = 2.54;
+var height, weight, name, age, goal, stats, measure;
+var calories = 0;
 
 // Capture form and listen for submit action
 var statsForm = document.getElementById('stats');
@@ -116,16 +147,13 @@ statsForm.addEventListener('submit', statsCapture, false);
 // Capture measurement options in form and listen for and react to changes in choice
 var measureType = document.getElementsByName("measures");
 for (i = 0; i < measureType.length; i++){
-    measureType[i].addEventListener('click', function(e){
+    measureType[i].addEventListener('change', function(e){
         getMeasure(e);
     });        
 };
 
-var calories = tdeeCalc(bmrCalc(age, weight, height));
-
-
-// Capture goals form and listen for submit action. Load new page after submit
+// Capture Goal form and submit all stats and load Stats Summary Page
 var goalForm = document.getElementById('goals');
-var goal = goalForm.addEventListener('submit', getGoal, false);
-
-var stats = macrosCalc(calories, goal);
+goalForm.addEventListener('submit', function(e){
+    submitStats(e);
+});
