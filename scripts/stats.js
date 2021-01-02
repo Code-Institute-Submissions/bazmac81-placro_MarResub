f02a88c0bcb0015614da254d1f404d59355d288e
bazmac81 <bazmac81@gmail.com>
2021-01-02 15:14:39 +0000

    Update: Save metric values for cals in getMeasures function

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,5 +1,4 @@
-// Calculate Calories
-
+// Calculate Calories and macros using user stats and goal choice
 function macrosCalc(cals, g) {
     var protMulti, carbMulti, fatMulti, protein, carbs, fat = 0;
 
@@ -29,6 +28,7 @@ function macrosCalc(cals, g) {
     return stats;
 };
 
+// Using user stats calculate Base Metabolic Rate
 function bmrCalc(a, w, h) {
     if(typeof(a) == "number" && typeof(w) == "number" && typeof(h) == "number") {
         if(a > 0 && h > 0 && w > 0){
@@ -44,16 +44,21 @@ function bmrCalc(a, w, h) {
     }
 };
 
+// Take BMR and return with daily calorie amount
 function tdeeCalc(bmr){
     tdee = Math.ceil(bmr * 1.5);
 
     return tdee;
 };
 
+// Submit initial form. Hide seciton and reveal goals section.
 function statsCapture(e){
     e.preventDefault();
+    name = document.getElementById('name').value;
+    age = document.getElementById('age').value;
 };
 
+// Capture goal selection. Store user data locally and proceed to next page
 function getGoal(e){
     e.preventDefault();
     
@@ -65,44 +70,50 @@ function getGoal(e){
             goalChoice = goalOptions[i].value;
         }
     };
-    console.log(goalChoice);
     return goalChoice;
 };
 
+// Manage user choice for measurement and update any inputs to reflect.
 function getMeasure(e){
     var m = e.target;
+    var h = document.getElementById('height');
+    var w = document.getElementById('weight');
     var measure = m.getAttribute('id');
-    var height = document.getElementById('height');
-    var weight = document.getElementById('weight');
 
     if(measure == "measure-met"){
-        if(weight.value != "") {
-            weight.value = Math.round(weight.value / lbsmulti);
+        if(w.value != "") {
+            w.value = Math.round(w.value / lbsmulti);
         };
-        if(height.value != "") {
-            height.value = Math.round(height.value * inchmulti); 
+        if(h.value != "") {
+            h.value = Math.round(h.value * inchmulti); 
         };
         document.getElementById('weight').setAttribute('placeholder', 'Weight kg');
         document.getElementById('height').setAttribute('placeholder', 'Height cm');
     }
     else {
-        if(weight.value != "") {
-            weight.value = Math.round(weight.value * lbsmulti);
+        // Keep metric versions of weight and height for calculation but display imperial values in form
+        if(w.value != "") {
+            weight = w.value;
+            w.value = Math.round(w.value * lbsmulti);
         };
-        if (height.value != "") {
-            height.value = Math.round(height.value / inchmulti);
+        if (h.value != "") {
+            height = h.value;
+            h.value = Math.round(h.value / inchmulti);
         };
         document.getElementById('weight').setAttribute('placeholder', 'Weight lbs');
-        document.getElementById('height').setAttribute('placeholder', 'Height ins'); 
+        document.getElementById('height').setAttribute('placeholder', 'Height ins');
     };
 }
 
 const lbsmulti = 2.20462;
 const inchmulti = 2.54;
+var height, weight, name, age;
 
+// Capture form and listen for submit action
 var statsForm = document.getElementById('stats');
 statsForm.addEventListener('submit', statsCapture, false);
 
+// Capture measurement options in form and listen for and react to changes in choice
 var measureType = document.getElementsByName("measures");
 for (i = 0; i < measureType.length; i++){
     measureType[i].addEventListener('click', function(e){
@@ -110,8 +121,11 @@ for (i = 0; i < measureType.length; i++){
     });        
 };
 
-var goalForm = document.getElementById('goals');
-goal = goalForm.addEventListener('submit', getGoal, false);
+var calories = tdeeCalc(bmrCalc(age, weight, height));
 
-var calories = tdeeCalc(bmrCalc(39, 80, 170));
-var macros = macrosCalc(calories, goal);
+
+// Capture goals form and listen for submit action. Load new page after submit
+var goalForm = document.getElementById('goals');
+var goal = goalForm.addEventListener('submit', getGoal, false);
+
+var stats = macrosCalc(calories, goal);
