3cfe98695f327a1814ed17fdc508a9f3e8dd5bbe
bazmac81 <bazmac81@gmail.com>
2021-01-09 20:58:21 +0000

    Add: Show user message if not recipes have been selected

--- scripts/mealplan.js
+++ scripts/mealplan.js
@@ -8,24 +8,32 @@ function postPlan(){
     mealPcCalc();
 
     document.getElementById('planHeader').textContent = userStats.name + "'s Meal Plan";
-
-    for (i = 0; i < meals.length; i++){
+    if(mealPlan.length === 0){
         section += 
             '<div>'+
-                '<h3 class="heading">'+meals[i]+'</h3>'+
-                '<ul class="mealplan-list">';
-        
-        for(j = 0; j < mealPlan.length; j++){
-            if(mealPlan[j].type === meals[i].toLowerCase()){  
-                section+=
-                    '<li class="mealplan-item">'+
-                        '<button class="remove"><i class="fa fa-times-circle"></i></button><span class="recipe-name">'+ mealPlan[j].name +'</span>'+
-                    '</li>';
-            };
-        };
-        section+= 
-                '</ul>'+
+                '<h3 class="heading">Your meal plan is empty</h3>'+
+                '<p>Choose a meal option from the menu or click the meal icon above and start selecting recipes to add to your plan.</p>'+
             '</div>';
+    }
+    else {
+        for (i = 0; i < meals.length; i++){
+            section += 
+                '<div>'+
+                    '<h3 class="heading">'+meals[i]+'</h3>'+
+                    '<ul class="mealplan-list">';
+            
+            for(j = 0; j < mealPlan.length; j++){
+                if(mealPlan[j].type === meals[i].toLowerCase()){  
+                    section+=
+                        '<li class="mealplan-item">'+
+                            '<button class="remove"><i class="fa fa-times-circle"></i></button><span class="recipe-name">'+ mealPlan[j].name +'</span>'+
+                        '</li>';
+                };
+            };
+            section+= 
+                    '</ul>'+
+                '</div>';
+        };
     };
 
     planList.innerHTML = section;
